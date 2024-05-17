b7f4abd99f6da0365661563cf2d50c6c19c05cb5
jkcachero <johnguitarizta@gmail.com>
2024-05-17 20:37:02 +0800

    Add title field

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,6 +7,7 @@ import SearchBox from './components/search-box/search-box.component'
 
 const App = () => {
   const [searchField, setSearchField] = useState('');
+  const [title, setTitleField] = useState('Monsters Rolodex');
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
@@ -31,14 +32,28 @@ const App = () => {
     setSearchField(searchFieldString)
   };
 
+  const onTitleChange = (event) => {
+    const titleFieldString = event.target.value.toLowerCase()
+
+    setTitleField(titleFieldString)
+  };
+
   return (
     <div className="App">
-      <h1 className="app-title">Monsters Rolodex</h1>
+      <h1 className="app-title">{ title }</h1>
 
       <SearchBox
         className='monsters-search-box'
         onChangeHandler={onSearchChange}
         placeholder='Search Monsters'
+      />
+
+      <br />
+
+      <SearchBox
+        className='title-search-box'
+        onChangeHandler={onTitleChange}
+        placeholder='Set Title'
       />
 
       <CardList monsters={filteredMonsters}/>
