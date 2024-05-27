45f92a5d35800fb99c56819d0f18fe26ed351ff1
jkcachero <johnguitarizta@gmail.com>
2024-05-27 14:33:39 +0800

    Add react-router-dom

--- the-store/src/App.js
+++ the-store/src/App.js
@@ -1,8 +1,24 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Home from './routes/home.component';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
 
 const App = () => {
-  return ( 
-    <Directory />
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
   );
 };
 
